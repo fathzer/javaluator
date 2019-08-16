bfe84f2749e77790b1a29da825505f99f5ca02fa
fathzer <fathzer@1b6e6dd2-b201-4fed-8f75-88e02abb1fff>
2019-08-16 17:03:46 +0000

    version 3.0.3

--- javaluator/src/test/java/com/fathzer/soft/javaluator/junit/DoubleEvaluatorTest.java
+++ javaluator/src/test/java/com/fathzer/soft/javaluator/junit/DoubleEvaluatorTest.java
@@ -9,7 +9,7 @@ import com.fathzer.soft.javaluator.StaticVariableSet;
 import com.fathzer.soft.javaluator.DoubleEvaluator.Style;
 
 public class DoubleEvaluatorTest {
-	private static DoubleEvaluator evaluator = new DoubleEvaluator();
+	private static final DoubleEvaluator EVALUATOR = new DoubleEvaluator();
 	
 	@Test
 	public void excelLike() {
@@ -19,42 +19,42 @@ public class DoubleEvaluatorTest {
 
 	@Test
 	public void testResults() {
-		assertEquals(-2, evaluator.evaluate("2+-2^2"),0.001);
-		assertEquals(2, evaluator.evaluate("6 / 3"),0.001);
-		assertEquals(Double.POSITIVE_INFINITY, evaluator.evaluate("2/0"),0.001);
-		assertEquals(2, evaluator.evaluate("7 % 2.5"),0.001);
-		assertEquals(-1., evaluator.evaluate("-1"), 0.001);
-		assertEquals(1., evaluator.evaluate("1"), 0.001);
-		assertEquals(-3, evaluator.evaluate("1+-4"), 0.001);
-		assertEquals(2, evaluator.evaluate("3-1"), 0.001);
-		assertEquals(-4, evaluator.evaluate("-2^2"),0.001);
-		assertEquals(2, evaluator.evaluate("4^0.5"),0.001);
+		assertEquals(-2, EVALUATOR.evaluate("2+-2^2"),0.001);
+		assertEquals(2, EVALUATOR.evaluate("6 / 3"),0.001);
+		assertEquals(Double.POSITIVE_INFINITY, EVALUATOR.evaluate("2/0"),0.001);
+		assertEquals(2, EVALUATOR.evaluate("7 % 2.5"),0.001);
+		assertEquals(-1., EVALUATOR.evaluate("-1"), 0.001);
+		assertEquals(1., EVALUATOR.evaluate("1"), 0.001);
+		assertEquals(-3, EVALUATOR.evaluate("1+-4"), 0.001);
+		assertEquals(2, EVALUATOR.evaluate("3-1"), 0.001);
+		assertEquals(-4, EVALUATOR.evaluate("-2^2"),0.001);
+		assertEquals(2, EVALUATOR.evaluate("4^0.5"),0.001);
 		
-		assertEquals(1, evaluator.evaluate("sin ( pi /2)"),0.001);
-		assertEquals(-1, evaluator.evaluate("cos(pi)"),0.001);
-		assertEquals(1, evaluator.evaluate("tan(pi/4)"),0.001);
-		assertEquals(Math.PI, evaluator.evaluate("acos( -1)"),0.001);
-		assertEquals(Math.PI/2, evaluator.evaluate("asin(1)"),0.001);
-		assertEquals(Math.PI/4, evaluator.evaluate("atan(1)"),0.001);
+		assertEquals(1, EVALUATOR.evaluate("sin ( pi /2)"),0.001);
+		assertEquals(-1, EVALUATOR.evaluate("cos(pi)"),0.001);
+		assertEquals(1, EVALUATOR.evaluate("tan(pi/4)"),0.001);
+		assertEquals(Math.PI, EVALUATOR.evaluate("acos( -1)"),0.001);
+		assertEquals(Math.PI/2, EVALUATOR.evaluate("asin(1)"),0.001);
+		assertEquals(Math.PI/4, EVALUATOR.evaluate("atan(1)"),0.001);
 		
-		assertEquals(1, evaluator.evaluate("ln(e)"),0.001);
-		assertEquals(2, evaluator.evaluate("log(100)"),0.001);
-		assertEquals(-1, evaluator.evaluate("min(1,-1)"),0.001);
-		assertEquals(-1, evaluator.evaluate("min(8,3,1,-1)"),0.001);
-		assertEquals(11, evaluator.evaluate("sum(8,3,1,-1)"),0.001);
-		assertEquals(3, evaluator.evaluate("avg(8,3,1,0)"),0.001);
+		assertEquals(1, EVALUATOR.evaluate("ln(e)"),0.001);
+		assertEquals(2, EVALUATOR.evaluate("log(100)"),0.001);
+		assertEquals(-1, EVALUATOR.evaluate("min(1,-1)"),0.001);
+		assertEquals(-1, EVALUATOR.evaluate("min(8,3,1,-1)"),0.001);
+		assertEquals(11, EVALUATOR.evaluate("sum(8,3,1,-1)"),0.001);
+		assertEquals(3, EVALUATOR.evaluate("avg(8,3,1,0)"),0.001);
 		
-		assertEquals(3, evaluator.evaluate("abs(-3)"),0.001);
-		assertEquals(3, evaluator.evaluate("ceil(2.45)"),0.001);
-		assertEquals(2, evaluator.evaluate("floor(2.45)"),0.001);
-		assertEquals(2, evaluator.evaluate("round(2.45)"),0.001);
+		assertEquals(3, EVALUATOR.evaluate("abs(-3)"),0.001);
+		assertEquals(3, EVALUATOR.evaluate("ceil(2.45)"),0.001);
+		assertEquals(2, EVALUATOR.evaluate("floor(2.45)"),0.001);
+		assertEquals(2, EVALUATOR.evaluate("round(2.45)"),0.001);
 		
-		double rnd = evaluator.evaluate("random()");
+		double rnd = EVALUATOR.evaluate("random()");
 		assertTrue(rnd>=0 && rnd<=1.0);
 
-		assertEquals(evaluator.evaluate("tanh(5)"), evaluator.evaluate("sinh(5)/cosh(5)"),0.001);
+		assertEquals(EVALUATOR.evaluate("tanh(5)"), EVALUATOR.evaluate("sinh(5)/cosh(5)"),0.001);
 		
-		assertEquals(-1, evaluator.evaluate("min(1,min(3+2,2))+-round(4.1)*0.5"),0.001);
+		assertEquals(-1, EVALUATOR.evaluate("min(1,min(3+2,2))+-round(4.1)*0.5"),0.001);
 	}
 	
 	@Test
@@ -63,39 +63,44 @@ public class DoubleEvaluatorTest {
 		StaticVariableSet<Double> variables = new StaticVariableSet<Double>();
 		
 		variables.set("x", 1.);
-		assertEquals(3, evaluator.evaluate(expression, variables),0.001);
+		assertEquals(3, EVALUATOR.evaluate(expression, variables),0.001);
 		variables.set("x", -1.);
-		assertEquals(1, evaluator.evaluate(expression, variables),0.001);
+		assertEquals(1, EVALUATOR.evaluate(expression, variables),0.001);
 	}
 	
 	@Test (expected=IllegalArgumentException.class)
 	public void test2ValuesFollowing() {
-		evaluator.evaluate("10 5 +");
+		EVALUATOR.evaluate("10 5 +");
 	}
 
 	@Test (expected=IllegalArgumentException.class)
 	public void test2ValuesFollowing2() {
-		evaluator.evaluate("(10) (5)");
+		EVALUATOR.evaluate("(10) (5)");
 	}
 
 	@Test (expected=IllegalArgumentException.class)
 	public void test2OperatorsFollowing() {
-		evaluator.evaluate("10**5");
+		EVALUATOR.evaluate("10**5");
 	}
 
 	@Test (expected=IllegalArgumentException.class)
 	public void testMissingEndValue() {
-		evaluator.evaluate("10*");
+		EVALUATOR.evaluate("10*");
 	}
 	
 	@Test (expected=IllegalArgumentException.class)
 	public void testNoFunctionArgument() {
-		evaluator.evaluate("sin()");
+		EVALUATOR.evaluate("sin()");
+	}
+
+	@Test (expected=IllegalArgumentException.class)
+	public void testNoFunctionArgument2() {
+		EVALUATOR.evaluate("average()");
 	}
 
 	@Test (expected=IllegalArgumentException.class)
 	public void testEmptyFunctionArgument() {
-		evaluator.evaluate("min(,2)");
+		EVALUATOR.evaluate("min(,2)");
 	}
 
 	@Test (expected=IllegalArgumentException.class)
@@ -110,47 +115,48 @@ public class DoubleEvaluatorTest {
 	
 	@Test
 	public void TestScientificNotation() {
+		DoubleEvaluator evaluator = new DoubleEvaluator(DoubleEvaluator.getDefaultParameters(), true);
 		assertEquals(3e-2, evaluator.evaluate("3e-2"), 1e-5);
 		assertEquals(30.9317695E+115, evaluator.evaluate("30.9317695E+115"), 1.0);
 	}
 
 	@Test (expected=IllegalArgumentException.class)
 	public void testSomethingBetweenFunctionAndOpenBracket() {
-		evaluator.evaluate("sin3(45)");
+		EVALUATOR.evaluate("sin3(45)");
 	}
 	
 	@Test (expected=IllegalArgumentException.class)
 	public void testMissingArgument() {
-		evaluator.evaluate("min(1,)");
+		EVALUATOR.evaluate("min(1,)");
 	}
 	
 	@Test (expected=IllegalArgumentException.class)
 	public void testInvalidArgumentACos() {
-		evaluator.evaluate("acos(2)");
+		EVALUATOR.evaluate("acos(2)");
 	}
 	
 	@Test (expected=IllegalArgumentException.class)
 	public void testInvalidArgumentASin() {
-		evaluator.evaluate("asin(2)");
+		EVALUATOR.evaluate("asin(2)");
 	}
 	
 	@Test (expected=IllegalArgumentException.class)
 	public void testOnlyCloseBracket() {
-		evaluator.evaluate(")");
+		EVALUATOR.evaluate(")");
 	}
 
 	@Test (expected=IllegalArgumentException.class)
 	public void testDSuffixInLiteral() {
-		evaluator.evaluate("3d+4");
+		EVALUATOR.evaluate("3d+4");
 	}
 
 	@Test (expected=IllegalArgumentException.class)
 	public void testStartWithFunctionSeparator() {
-		evaluator.evaluate(",3");
+		EVALUATOR.evaluate(",3");
 	}
 
 	@Test (expected=IllegalArgumentException.class)
 	public void testNoArgInAverageFunction() {
-		evaluator.evaluate("avg()");
+		EVALUATOR.evaluate("avg()");
 	}
 }
