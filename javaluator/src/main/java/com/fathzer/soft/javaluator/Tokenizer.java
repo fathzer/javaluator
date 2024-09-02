6ba0af5943a7cd17f9ea49efb7863159daad2718
Astesana <jm-dev@astesana.net>
2024-09-02 10:45:07 +0200

    Fixes infer type arguments in anon inner class (not supported java 8)

--- javaluator/src/main/java/com/fathzer/soft/javaluator/Tokenizer.java
+++ javaluator/src/main/java/com/fathzer/soft/javaluator/Tokenizer.java
@@ -70,7 +70,7 @@ public class Tokenizer {
 		// the longer may be before the shorter (&& should be before &) or the regexpr
 		// parser will recognize && as two &.
 		//TODO Create a test to verify that operators that includes others are working
-		Collections.sort(delimiters, new Comparator<>() {
+		Collections.sort(delimiters, new Comparator<String>() {
 			@Override
 			public int compare(String o1, String o2) {
 				return o2.compareTo(o1);
